c824a9b0a060d03ff075cccdae84d951f53fd62a
Goneflash <fz.zhangfan@gmail.com>
2017-03-26 00:05:10 -0700

    Testing tests

--- feature_tracker/test/feature_tracker_test.cpp
+++ feature_tracker/test/feature_tracker_test.cpp
@@ -1,10 +1,19 @@
+#include <opencv2/opencv.hpp>
+
 #include "gtest/gtest.h"
 
 class FeatureTrackerTest : public ::testing::Test {};
 
 TEST(FeatureTrackerTest, TestTwoFrame) {
-  ASSERT_NE(20, 10);
-  ASSERT_EQ(20, 20);
+  ASSERT_TRUE(1);
+}
+
+TEST(FeatureTrackerTest, TestTwoFrame1) {
+  cv::Mat image0 = cv::imread("../feature_tracker/test_data/frame0.png");
+  cv::Mat image1 = cv::imread("../feature_tracker/test_data/frame1.png");
+
+  ASSERT_TRUE(image0.data);
+  ASSERT_TRUE(image1.data);
 }
 
 int main(int argc, char **argv) {
