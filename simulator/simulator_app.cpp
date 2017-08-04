0d7b206380b488a4503376f75e8d17e23ea3dac8
Goneflash <fz.zhangfan@gmail.com>
2017-08-03 17:12:25 -0700

    Simulator now loads camera model

--- simulator/simulator_app.cpp
+++ simulator/simulator_app.cpp
@@ -12,16 +12,33 @@ struct Options {
 };
 
 bool LoadCameraModel(const cv::FileNode &node, vio::Scene &scene) {
-  const std::string camera_type = (std::string)node["Camera Type"];
+  const std::string camera_type = (std::string)node["CameraType"];
   if (camera_type == "Pinhole") {
+    // Load parameters.
+    int num_params = (int)node["NumParams"];
+    cv::Mat params_cv;
+    node["Params"] >> params_cv;
     vio::PinholeCameraModel<double>::ParamsArray params;
+    // TODO: Check size of params == num_params.
+    for (int i = 0; i < num_params; ++i) {
+      params[i] = params_cv.at<double>(i);
+      std::cout << "Param " << i << " : " << params[i] << std::endl;
+    }
+
     std::unique_ptr<vio::CameraModel<double>> camera =
         std::unique_ptr<vio::CameraModel<double>>(
             new vio::PinholeCameraModel<double>(480, 640, params));
     scene.SetCameraModel(std::move(camera));
   } else {
+    std::cerr << "Error: Unknown camera model.\n";
     return false;
   }
+  return true;
+}
+
+bool LoadLandmarks(const std::string &scene_file_name,
+                             vio::Scene &scene) {
+
   return true;
 }
 
@@ -63,6 +80,10 @@ int main(int argc, char **argv) {
     return false;
 
   vio::Simulator simulator;
+  std::vector<std::vector<Eigen::Vector2d>> feature_pos_each_frame;
+  if (!simulator.GenerateFeatureTracksFromTranslationTrajectory(
+        scene, feature_pos_each_frame))
+    return false;
 
   return 0;
 }
