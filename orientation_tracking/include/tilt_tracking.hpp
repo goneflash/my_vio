8c77d81d0d31ba93caf84347dda21e097c136b56
Goneflash <fz.zhangfan@gmail.com>
2017-11-21 16:41:58 -0800

    working on tilt tracking quaternion

--- orientation_tracking/include/tilt_tracking.hpp
+++ orientation_tracking/include/tilt_tracking.hpp
@@ -5,7 +5,11 @@
 
 #include <Eigen/Dense>
 
+#include "imu_integrator.hpp"
+
 namespace vio {
+
+// TODO: Don't need a class for that.
 
 /*
  * Coordinate is Right-handed:
@@ -26,11 +30,14 @@ namespace vio {
  *  Followed this tutorial:
  *  EE 267 Virtual Reality
  *  Course Notes: 3-DOF Orientation Tracking with IMUs
+ *
+ *
+ *  Quaternion is in the order: x, y, z, w
  */
 template <typename TYPE>
 class TiltTracker {
  public:
-  TiltTracker() : g_(9.81) {}
+  TiltTracker() : g_(9.81), alpha_(0.5) {}
 
   // Acceleration is represented:
   //     m/s^2
@@ -39,8 +46,26 @@ class TiltTracker {
   bool GetRollPitch(TYPE acc_x, TYPE acc_y, TYPE acc_z, TYPE &roll,
                     TYPE &pitch);
 
+  // From paragraph 5.2.
+  // Equation (23)
+  // Format is : [w x y z]
+  bool GetCorrectionQuaternion(TYPE acc_x, TYPE acc_y, TYPE acc_z,
+                               Eigen::Vector4d &q_correct);
+
+  // From paragraph 5.3.
+  bool CorrectOrientationInTilt(TYPE acc_x, TYPE acc_y, TYPE acc_z,
+                                const Eigen::Vector4d &q_cur,
+                                Eigen::Vector4d &q_correct);
+
+  void set_tilt_correction_alpha(double alpha) { alpha_ = alpha; }
+
  private:
   TYPE g_;
+
+  // Parameter for complimentary filter.
+  double alpha_;
+
+  ImuIntegrator imu_integrator_;
 };
 
 template <typename TYPE>
@@ -59,6 +84,48 @@ bool TiltTracker<TYPE>::GetRollPitch(TYPE acc_x, TYPE acc_y, TYPE acc_z,
   else
     pitch =
         -atan2(-acc_z_g, acc_y_g * sqrt(acc_x_g * acc_x_g + acc_y_g * acc_y_g));
+
+  return true;
+}
+
+template <typename TYPE>
+bool TiltTracker<TYPE>::GetCorrectionQuaternion(TYPE acc_x, TYPE acc_y,
+                                                TYPE acc_z,
+                                                const Eigen::Vector4d &q_cur
+                                                    Eigen::Vector4d &q_tilt) {
+  // See chapter 5.3
+  // Get acceleration quaternion in body frame.
+  // TODO: The order of w, x, y, z
+  Eigen::Vector4d q_acc_body(acc_x, acc_y, acc_z, 0);
+  Eigen::Vector4d q_acc_body = q_acc_body.normalized();
+
+  // Get acceleration quaternion in world frame.
+
+  Eigen::Vector3d acc(acc_x, acc_y, acc_z);
+  Eigen::Vector3d normalized_acc = acc.normalized();
+  Eigen::Vector3d rotation_vector(-normalized_acc(2), 0, normalized_acc(0));
+
+  q_tilt =
+      vector_angle_to_quaternion(arccos(normalized_acc(1)), rotation_vector);
+  return true;
+}
+
+template <typename TYPE>
+bool TiltTracker<TYPE>::CorrectOrientationInTilt(TYPE acc_x, TYPE acc_y,
+                                                 TYPE acc_z,
+                                                 const Eigen::Vector3d &omega,
+                                                 double delta_t,
+                                                 const Eigen::Vector4d &q_cur,
+                                                 Eigen::Vector4d &q_corrected) {
+  // Propagate gyro data.
+  // In this course, they use delta_q * p * delta_q_inverse to apply the
+  // rotation which is not correct based on IMU model.
+  Eigen::Vector4d updated_q;
+  imu_integrator_.ZerothOrderIntegration(q_cur, omega, delta_t, updated_q);
+
+  Eigen::Vector4d q_tilt_correction;
+  if (!GetCorrectionQuaternion(acc_x, acc_y, acc_z, q_tilt_correction))
+    return false;
 
   return true;
 }
--- util/include/math_helper.hpp
+++ util/include/math_helper.hpp
@@ -5,12 +5,15 @@
 
 // TODO: Need tests.
 
-// Quaternion format : [x, y, z, w]
-// q x p =
-// [ q4p1 + q3p2 - q2p3 + q1p4
-//  -q3p1 + q4p2 + q1p3 + q2p4
-//   q2p1 - q1p2 + q4p3 + q3p4
-//  -q1p1 - q2p2 - q3p3 + q4p4 ]
+/* Quaternion format : [x, y, z, w]
+* q x p =
+* [ q4p1 + q3p2 - q2p3 + q1p4
+*  -q3p1 + q4p2 + q1p3 + q2p4
+*   q2p1 - q1p2 + q4p3 + q3p4
+*  -q1p1 - q2p2 - q3p3 + q4p4 ]
+*
+*  Most cases here is delta_q * p.
+*/
 inline Eigen::Vector4d quaternion_multi(const Eigen::Vector4d &q,
                                         const Eigen::Vector4d &p) {
   Eigen::Vector4d result;
@@ -20,6 +23,26 @@ inline Eigen::Vector4d quaternion_multi(const Eigen::Vector4d &q,
       -q(0) * p(0) - q(1) * p(1) + q(2) * p(2) + q(3) * p(3);
   return result;
 }
+
+/*
+ * Order: x, y, z, w
+ */
+inline Eigen::Vector4d inverse_quaternion(const Eigen::Vector4d &p) {
+  Eigen::Vector4d normalized_p = p.normalized();
+  Eigen::Vector4d inv_p(-normalized_p(0), -normalized_p(1), -normalized_p(2),
+                        normalized_p(3));
+  return inv_p;
+}
+
+
+inline Eigen::Vector4d conjugate_quaternion(const Eigen::Vector4d &p) {}
+
+/*
+ * Rotate vector p by rotation q
+ * output = q * p * q^-1
+ */
+inline Eigen::Vector4d rotate_vector(const Eigen::Vector4d &p,
+                                     const Eigen::Vector4d &q) {}
 
 /*
  * [q1, q2, q3]' -->
@@ -46,6 +69,19 @@ inline Eigen::MatrixXd omega_matrix(const Eigen::Vector3d &w) {
   m << 0, w(2), -w(1), w(0), -w(2), 0, w(0), w(1), w(1), -w(0), 0, w(2), -w(0),
       -w(1), -w(2), 0;
   return m;
+}
+
+/*
+ * q(theta, v) =
+ *    cos(theta / 2), v_x * sin(theta / 2), vy * sin(theta / 2),
+ *    vz * sin(theta / 2)
+ */
+inline Eigen::Vector4d vector_angle_to_quaternion(double angle,
+                                                  const Eigen::Vector3d &v) {
+  Eigen::Vector4d q;
+  q << cos(angle / 2), v(0) * sin(angle / 2), v(1) * sin(angle / 2),
+      v(2) * sin(angle / 2);
+  return q;
 }
 
 #endif  // UTIL_MATH_HELPER_HPP_
